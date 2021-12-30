4276827776b1b2d840d4f38bb15b2040cb6791b2
Hassan-Idressi <96679440+Hassan-Idressi@users.noreply.github.com>
2021-12-31 00:25:29 +0500

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+const joi = require('@hapi/joi');
+const { string } = require("@hapi/joi");
+
+var productsSchema= mongoose.Schema({
+
+name:String,
+price:Number,
+company:String,
+})
+
+function valditaionProduct(data){
+
+
+    const Schema= joi.object({
+     name: joi.string().min(3).max(10).required(),
+     price: joi.number().min(0).required(),
+     company: joi.string().min(3).max(10).required(),
+    })
+    return Schema.validate(data,{abortEarly:false});
+}
+var Product = mongoose.model("Product",productsSchema);
+
+module.exports.Product=Product;
+module.exports.validate = valditaionProduct;
